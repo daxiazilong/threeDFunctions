664a611e26a2aad9dabcefa66375ac813e5ad12c
fengruxian <745629882@qq.com>
2019-04-30 11:05:35 +0800

    * 添加了一些基本形状

--- src/paint/DrawFunction.ts
+++ src/paint/DrawFunction.ts
@@ -5,19 +5,47 @@ class DrawFunctions{
         this.draw( Painter )
     }
     draw(Painter){
-        let geometry = (new THREE.ParametricGeometry( this.gen3DVertical , 25, 25)) ;
+        let geometry = (new THREE.ParametricGeometry( this.genCube , 25, 25)) ;
         let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 ,side:THREE.DoubleSide} );
+        material.wireframe  = true;
+
         let klein:THREE.Mesh = new THREE.Mesh( geometry, material );
         Painter.scene.add( klein )
     }
 
 
 
-    gen3DVertical(u,v, target:THREE.Vector3  ){
-        var x = v*500;
-        var z = 0;
-        var y =0;
-        target.set(x, 200, z);
+    genParaboloid (u,v, target:THREE.Vector3  ){
+        let k = 500;//x、y取值范围
+        let a = 0.1;//旋转抛物面焦点
+        let x = (u-0.5) * k;
+        let z = (v-0.5) * k;
+        let y = Math.pow(a, 2) * (Math.pow(x, 2) + Math.pow(z, 2));
+        target.set(x, y, z);
+    }
+
+    genCircle( u,v, target:THREE.Vector3 ){
+        u = u*Math.PI*2;
+        v = v*Math.PI*2;
+
+        let r = 250;
+        let x = r*Math.cos(u);
+
+        let y = Math.sqrt( Math.pow(r,2) - Math.pow(x,2) )*Math.sin(v);
+        
+        target.set(x, y, 0);
+    }
+    genCube(u,v, target:THREE.Vector3){
+        // 球面坐标
+        u = u*Math.PI;
+        v = v*Math.PI*2;
+
+        let r = 500;
+
+        let x = r*Math.sin( u )*Math.cos(v);
+        let z = r*Math.sin( u )*Math.sin(v);
+        let y = r*Math.cos(u);
+        target.set(x, y, z);
     }
 
 
